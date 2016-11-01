db9eeb9278790d4a4f77c10e7357a21f9dc1ff9f
pengzhuo <pch987.net@163.com>
2016-11-01 11:17:11 +0800

    1. 缩小FB点击区域

--- app/src/main/java/com/duduws/ads/view/Facebook_Native_Activity.java
+++ app/src/main/java/com/duduws/ads/view/Facebook_Native_Activity.java
@@ -8,6 +8,7 @@ import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -34,11 +35,12 @@ import java.util.Random;
 
 /**
  * Facebook原生广告(即时请求)
+ *
  * @author Pengz
  * @mail pch987.net@163.com
  * @time 16/8/23 20:39
  */
-public class Facebook_Native_Activity extends BaseActivity implements AdListener{
+public class Facebook_Native_Activity extends BaseActivity implements AdListener {
     private static final String TAG = "Facebook_Native_Activity";
     private static long timeDelay = 0;
     private int triggerType = -1;
@@ -61,8 +63,8 @@ public class Facebook_Native_Activity extends BaseActivity implements AdListener
             isOutSide = intent.getExtras().getBoolean(DspHelper.AD_EXTRA_SITE);
         }
 
-        if (triggerType == ConstDefine.TRIGGER_TYPE_APP_ENTER){
-            if (savedInstanceState == null){
+        if (triggerType == ConstDefine.TRIGGER_TYPE_APP_ENTER) {
+            if (savedInstanceState == null) {
                 savedInstanceState = new Bundle();
             }
             savedInstanceState.putBoolean("TRIGGER_TYPE_APP_ENTER", true);
@@ -73,16 +75,16 @@ public class Facebook_Native_Activity extends BaseActivity implements AdListener
 
         offset = DspHelper.getTriggerOffSet(triggerType);
 
-        site = DspHelper.getDspSite(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE+offset);
+        site = DspHelper.getDspSite(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE + offset);
 
-        if (!TextUtils.isEmpty(site)){
+        if (!TextUtils.isEmpty(site)) {
             //加载原生广告
             nativeAd = new NativeAd(this, site);
             nativeAd.setAdListener(this);
             nativeAd.loadAd(NativeAd.MediaCacheFlag.ALL);
 
             AnalyticsUtils.onEvent(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE, triggerType, ConstDefine.AD_TYPE_NATIVE_SPOT, ConstDefine.AD_RESULT_REQUEST);
-        }else{
+        } else {
             finish();
         }
     }
@@ -90,19 +92,19 @@ public class Facebook_Native_Activity extends BaseActivity implements AdListener
     @Override
     public void onError(Ad ad, AdError adError) {
         MLog.e(TAG, "facebook native ad error! code: " + adError.getErrorCode() + ", message: " + adError.getErrorMessage());
-        if (!isOutSide){
-            int triesNum = DspHelper.getDspSiteTriesNum(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE+offset) + 1;
-            DspHelper.setDspSiteTriesNum(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE+offset, triesNum);
-            int totalNum = DspHelper.getDspSiteTotalTriesNum(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE+offset);
-            if (triesNum >= totalNum){
-                DspHelper.setDspSiteTriesFlag(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE+offset, true);
-                DspHelper.setDspSiteTriesTime(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE+offset, System.currentTimeMillis());
+        if (!isOutSide) {
+            int triesNum = DspHelper.getDspSiteTriesNum(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE + offset) + 1;
+            DspHelper.setDspSiteTriesNum(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE + offset, triesNum);
+            int totalNum = DspHelper.getDspSiteTotalTriesNum(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE + offset);
+            if (triesNum >= totalNum) {
+                DspHelper.setDspSiteTriesFlag(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE + offset, true);
+                DspHelper.setDspSiteTriesTime(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE + offset, System.currentTimeMillis());
             }
         }
         //重置广告展示标志
         DspHelper.setCurrentAdsShowFlag(this, false);
         AnalyticsUtils.onEvent(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE, triggerType, ConstDefine.AD_TYPE_NATIVE_SPOT, ConstDefine.AD_RESULT_FAIL);
-        if (triggerType == ConstDefine.TRIGGER_TYPE_APP_ENTER){
+        if (triggerType == ConstDefine.TRIGGER_TYPE_APP_ENTER) {
             finish();
         }
     }
@@ -111,28 +113,28 @@ public class Facebook_Native_Activity extends BaseActivity implements AdListener
     public void onAdLoaded(Ad ad) {
         MLog.i(TAG, "facebook native ad onAdLoaded!");
         AnalyticsUtils.onEvent(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE, triggerType, ConstDefine.AD_TYPE_NATIVE_SPOT, ConstDefine.AD_RESULT_SUCCESS);
-        if (nativeAd == null || nativeAd != ad){
+        if (nativeAd == null || nativeAd != ad) {
             return;
         }
         //重新延时返回键1秒
 //        timeDelay = System.currentTimeMillis() - (ConstDefine.BACK_KEY_DELAY_TIME-1)*1000;
         setContentView(R.layout.activity_native_ad);
-        nativeAdContainer = (LinearLayout)findViewById(R.id.native_ad_container);
+        nativeAdContainer = (LinearLayout) findViewById(R.id.native_ad_container);
         LayoutInflater inflater = LayoutInflater.from(this);
-        if (triggerType == ConstDefine.TRIGGER_TYPE_APP_ENTER){
-            adView = (RelativeLayout)inflater.inflate(R.layout.ad_unit, nativeAdContainer, false);
-        } else if (triggerType == ConstDefine.TRIGGER_TYPE_APP_EXIT){
-            adView = (RelativeLayout)inflater.inflate(R.layout.ad_unit_ex, nativeAdContainer, false);
+        if (triggerType == ConstDefine.TRIGGER_TYPE_APP_ENTER) {
+            adView = (RelativeLayout) inflater.inflate(R.layout.ad_unit, nativeAdContainer, false);
+        } else if (triggerType == ConstDefine.TRIGGER_TYPE_APP_EXIT) {
+            adView = (RelativeLayout) inflater.inflate(R.layout.ad_unit_ex, nativeAdContainer, false);
         } else {
-            adView = (RelativeLayout)inflater.inflate(R.layout.ad_unit, nativeAdContainer, false);
+            adView = (RelativeLayout) inflater.inflate(R.layout.ad_unit, nativeAdContainer, false);
         }
         nativeAdContainer.addView(adView);
 
         nativeAd.unregisterView();
         inflateAd(nativeAd, adView);
-        if (!isOutSide){
-            DspHelper.updateRequestData(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE+offset);
-            DspHelper.updateShowData(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE+offset);
+        if (!isOutSide) {
+            DspHelper.updateRequestData(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE + offset);
+            DspHelper.updateShowData(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE + offset);
         }
         AnalyticsUtils.onEvent(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE, triggerType, ConstDefine.AD_TYPE_NATIVE_SPOT, ConstDefine.AD_RESULT_SHOW);
     }
@@ -144,13 +146,13 @@ public class Facebook_Native_Activity extends BaseActivity implements AdListener
         finish();
     }
 
-    private void inflateAd(NativeAd nativeAd, View adView){
-        LinearLayout adChoice = (LinearLayout)adView.findViewById(R.id.ad_choice);
-        if (adChoicesView == null){
+    private void inflateAd(NativeAd nativeAd, View adView) {
+        LinearLayout adChoice = (LinearLayout) adView.findViewById(R.id.ad_choice);
+        if (adChoicesView == null) {
             adChoicesView = new AdChoicesView(this, nativeAd, true);
             adChoice.addView(adChoicesView, 0);
         }
-        delete = (ImageView)adView.findViewById(R.id.ad_delete);
+        delete = (ImageView) adView.findViewById(R.id.ad_delete);
 //        delete.setVisibility(View.INVISIBLE);
 //        delete.setEnabled(false);
 //        new Thread(new Runnable() {
@@ -181,6 +183,7 @@ public class Facebook_Native_Activity extends BaseActivity implements AdListener
         MediaView nativeAdMedia = (MediaView) adView.findViewById(R.id.native_ad_media);
         nativeAdMedia.setAutoplay(AdSettings.isVideoAutoplay());
         nativeAdMedia.setAutoplayOnMobile(AdSettings.isVideoAutoplayOnMobile());
+
         Button nativeAdCallToAction = (Button) adView.findViewById(R.id.native_ad_call_to_action);
         nativeAdCallToAction.setText(nativeAd.getAdCallToAction());
         nativeAdCallToAction.setVisibility(View.VISIBLE);
@@ -193,18 +196,26 @@ public class Facebook_Native_Activity extends BaseActivity implements AdListener
 
         nativeAdMedia.setNativeAd(nativeAd);
 
+        RelativeLayout mask = (RelativeLayout)adView.findViewById(R.id.mask_layout);
+        mask.setClickable(false);
+
         // Wire up the View with the native ad, the whole nativeAdContainer will be clickable.
-        RelativeLayout clickLayout = (RelativeLayout)adView.findViewById(R.id.clickLayout);
+        RelativeLayout clickLayout = (RelativeLayout) adView.findViewById(R.id.clickLayout);
+        RelativeLayout imgClickLayout = (RelativeLayout)adView.findViewById(R.id.click_layout);
+        ViewGroup.LayoutParams pp = imgClickLayout.getLayoutParams();
+        pp.height = clickLayout.getHeight()/2;
+        imgClickLayout.setLayoutParams(pp);
 //        nativeAd.registerViewForInteraction(clickLayout);
         List<View> viewList = new ArrayList<View>();
-        viewList.add(nativeAdMedia);
+//        viewList.add(nativeAdMedia);
+        viewList.add(imgClickLayout);
         viewList.add(nativeAdCallToAction);
         nativeAd.registerViewForInteraction(clickLayout, viewList);
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (System.currentTimeMillis() - timeDelay >= ConstDefine.BACK_KEY_DELAY_TIME*1000){
+        if (System.currentTimeMillis() - timeDelay >= ConstDefine.BACK_KEY_DELAY_TIME * 1000) {
             return super.onKeyDown(keyCode, event);
         }
         return false;
@@ -220,16 +231,16 @@ public class Facebook_Native_Activity extends BaseActivity implements AdListener
     }
 
     //延时弹出CM广告
-    private void delayShowCmAds(){
+    private void delayShowCmAds() {
         if (DspHelper.isDelayShowAdsEnable(this, ConstDefine.DSP_CHANNEL_FACEBOOK_NATIVE) &&
-                (triggerType != ConstDefine.TRIGGER_TYPE_APP_ENTER && triggerType != ConstDefine.TRIGGER_TYPE_APP_EXIT)){
+                (triggerType != ConstDefine.TRIGGER_TYPE_APP_ENTER && triggerType != ConstDefine.TRIGGER_TYPE_APP_EXIT)) {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
                         int time = ConstDefine.DELAY_TIME_AFTER_ADS + new Random().nextInt(30) + 1;
                         MLog.i(TAG, "delay time " + time + " to show ads!");
-                        Thread.sleep(time*1000);
+                        Thread.sleep(time * 1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -239,16 +250,16 @@ public class Facebook_Native_Activity extends BaseActivity implements AdListener
         }
     }
 
-    public static Handler handler = new Handler(){
+    public static Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            switch (msg.what){
+            switch (msg.what) {
                 case 0:
-                    if (delete != null){
-                        try{
+                    if (delete != null) {
+                        try {
                             delete.setVisibility(View.VISIBLE);
                             delete.setEnabled(true);
-                        }catch (Exception e){
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
